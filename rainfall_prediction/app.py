9adc173bc9ec9b6b773487a2c62319d955c3d3c7
prakhar17072004 <pmankur2004@gmail.com>
2025-10-03 01:41:37 +0530

    ui dashboard is updated

--- rainfall_prediction/app.py
+++ rainfall_prediction/app.py
@@ -5,35 +5,38 @@ import requests
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env
 load_dotenv()
 
 app = Flask(__name__)
-
-# Load trained ML model
 model = joblib.load('model.joblib')
-
-# Get API key from environment
-API_KEY = os.getenv("OPENWEATHER_API_KEY")
+API_KEY = os.getenv('OPENWEATHER_API_KEY')
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def dashboard():
     prediction = None
     weather_data = {}
-    
+    location_info = {}
+
+    city = "London"  # default city
+    country = ""
+
     if request.method == 'POST':
-        lat = request.form['lat']
-        lon = request.form['lon']
-        
-        url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric"
-        response = requests.get(url)
+        city = request.form['city']
+        country = request.form['country']
+
+    # Call OpenWeatherMap API
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={API_KEY}&units=metric"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
         data = response.json()
-        
         temperature = data['main']['temp']
         humidity = data['main']['humidity']
         pressure = data['main']['pressure']
         wind_speed = data['wind']['speed']
-        
+        country_code = data['sys']['country']
+        location_name = data['name']
+
         weather_data = {
             'temperature': temperature,
             'humidity': humidity,
@@ -41,11 +44,21 @@ def index():
             'wind_speed': wind_speed
         }
 
+        location_info = {
+            'city': location_name,
+            'country': country_code
+        }
+
+        # Predict rainfall
         input_features = np.array([[temperature, humidity, pressure, wind_speed]])
         prediction = model.predict(input_features)[0]
         prediction = round(prediction, 2)
-    
-    return render_template('index.html', prediction=prediction, weather=weather_data)
+    else:
+        weather_data = None
+        prediction = None
+        location_info = None
+
+    return render_template('dashboard.html', weather=weather_data, prediction=prediction, location=location_info)
 
 if __name__ == '__main__':
     app.run(debug=True)
